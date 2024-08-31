2454c5c02bb28e6ea2ee3d6eaac01631d29dfebc
NaaluZeroNaalu <ajithruflex1210@gmail.com>
2024-08-31 01:44:43 -0700

    added some responses

--- responses.js
+++ responses.js
@@ -1,10 +1,20 @@
 function getBotResponse(input) {
     
     // Simple responses
-    if (input == "hi") {
+    if (input == "hi" || input == "HI" || input == "Hi") {
         return "hi;)";
-    } else if (input == "who made this project") {
+    } else if (input == "who made this project" || input == "who made this website" || input == "Who made this project" || input == "Who made this website") {
         return "this project was made by ajith kumar yeah its me of course";
+    }else if(input == "what is your age" || input == "What is your age" || input == "Your age" || input == "your age" || input == "What is your age ajith" || input == "what is your age ajith" || input == "your age?" || input == "Your age?"){
+
+        const date = new Date()
+        return date.getFullYear() - 2000 
+    }else if(input == "you know reactjs" || input == "You know react js" || input == "you know java" || input == "You know java"){
+
+        return "yes i know"
+    }else if(input == "can i get your number" || input == "your number" || input == "Your number" || input == "Your number?" || input == "Your contact no" || input == "your contact no"){
+
+        return 7358629697
     }
      else {
         return "Sorry i don't understand";
